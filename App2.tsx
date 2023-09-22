d27df2da60be90259baa0673dc9c379413c5fe60
raylee1994 <849103991@qq.com>
2023-09-22 20:13:49 +0800

    test

--- App2.tsx
+++ App2.tsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
-import { TextInput, TextInputBase, View } from 'react-native';
+import React, {useState} from 'react';
+import {TextInput, View} from 'react-native';
 
 export default function App2() {
   const [value, setValue] = useState('');
   return (
     <View>
-      <TextInput value={value} />
+      <TextInput
+        value={value}
+        onChangeText={e => {
+          setValue(e);
+        }}
+      />
     </View>
   );
 }
